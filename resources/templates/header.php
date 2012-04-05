2779a25f0376bd9aef6cb0c1148417e13ed92259
Fredrik Teschke <fredrikteschke@gmail.com>
2012-04-05 08:01:23 +0200

    Layouting of whole page

---
+++ resources/templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Simple Site</title>
+	<link rel="stylesheet" href="css/main.css" type="text/css">
+</head>
+
+<body>
+<div id="topbar">
+	<h1>Lecturate</h1>
+</div>
+<div id="navbar">
+<a href="modul.php">Module</a>
+</div>
