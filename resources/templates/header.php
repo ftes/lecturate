26a9c6adc97c5094bdcf6bf4fc9a8054e99158c0
Julia <julia.lakatos@gmail.com>
2012-04-16 15:16:33 +0200

    CSS Datei Aktualisiert

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,21 +1,20 @@
 <div id="topbar">
-
-<div style="position: absolute; top: 0px; width: 740px; z-index: 2;">
-	<h1>Lecturate</h1>
-	<img src="img/logo.png" alt="DHBW-Logo" style="position:absolute; top: 20px; right:10px; height:80px;">
-	<p>Bewerten, Verbessern, Profitieren!</p>
-</div>
-<div style="position: absolute; top: 10px; z-index: 1; width:740px; background: white; opacity: 0.8; display: run-in; height: 80px; border-radius: 7px 7px 7px 7px;"></div>
+	<div id="contentTopBar">
+		<h1>Lecturate</h1>
+		<img id="logo" src="img/logo.png" alt="DHBW-Logo">
+		<p>Bewerten, Verbessern, Profitieren!</p>
+	</div>
+	<div id="background"></div>
 </div>
 <div id="navbar">
-<?=T::iconButton(false, "Admin", "admin")?>
-<?=T::iconButton(false, "Vorlesung", "lecture")?>
-<?=T::iconButton(false, "Dozent", "docent")?>
-<?=T::iconButton(false, "SGL", "advisor")?>
-<?=T::iconButton(false, "Kurs", "classs")?>
-<?=T::iconButton(false, "Dozent hält Vorlesung", "docent_lecture")?>
-<?=T::iconButton(false, "Kurs hört gehaltene Vorlesung", "classs_docent_lecture")?>
-<?=T::iconButton(false, "Einmal-PW", "otpw")?>
-<?=T::iconButton(false, "Bewertung", "rating")?>
-<?=T::iconButton(false, "Student", "student")?>
+	<?=T::iconButton(false, "Admin", "admin")?>
+	<?=T::iconButton(false, "Vorlesung", "lecture")?>
+	<?=T::iconButton(false, "Dozent", "docent")?>
+	<?=T::iconButton(false, "SGL", "advisor")?>
+	<?=T::iconButton(false, "Kurs", "classs")?>
+	<?=T::iconButton(false, "Dozent hält Vorlesung", "docent_lecture")?>
+	<?=T::iconButton(false, "Kurs hört gehaltene Vorlesung", "classs_docent_lecture")?>
+	<?=T::iconButton(false, "Einmal-PW", "otpw")?>
+	<?=T::iconButton(false, "Bewertung", "rating")?>
+	<?=T::iconButton(false, "Student", "student")?>
 </div>
