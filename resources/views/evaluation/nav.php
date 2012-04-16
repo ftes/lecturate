c63c873b0bdb90f3856f3fc921e801c99b4f16b6
Philipp Mail <mail.philipp@gmx.de>
2012-04-16 15:26:36 +0200

    Buttons

--- resources/views/evaluation/nav.php
+++ resources/views/evaluation/nav.php
@@ -1,2 +1,4 @@
-<?=T::iconButton(false, "DHBW Allgemein", "evaluation", "evaluateAll") ?>
-<?=T::iconButton(false, "Vorlesung", "evaluation", "evaluateDocentLecture") ?>
+<?=T::iconButton(false, "DHBW", "evaluation", "evaluateAll") ?>
+<?=T::iconButton(false, "Vorlesung", "lecture", "index") ?>
+<?=T::iconButton(false, "Dozent", "docent", "index") ?>
+<?=T::iconButton(false, "Dozent hält Vorlesung", "docentlecture", "index") ?>
